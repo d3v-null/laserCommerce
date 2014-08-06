8b906d25c623043bf5fb39e10b752da31d373bae
derwentx <derwentx@gmail.com>
2014-08-07 05:16:22 +0800

    Writes input boxes to form

--- lasercommerce/Lasercommerce_Plugin.php
+++ lasercommerce/Lasercommerce_Plugin.php
@@ -62,8 +62,6 @@ class Lasercommerce_Plugin extends Lasercommerce_LifeCycle {
     }
     
     protected function otherInstall(){ //overrides abstract in parent LifeCycle
-        If(WP_DEBUG) error_log("called otherinstall\n");
-        //TODO:
     }
 
     /**
@@ -93,32 +91,112 @@ class Lasercommerce_Plugin extends Lasercommerce_LifeCycle {
         // if( is_admin() ){
             // add_filter( 'woocommerce_get_settings_pages', array($this, 'includeAdminPage') );
         // }
-        
-        //TODO:
-        //register product price hooks
-        //register cart hooks
-        
     }
     
+    
+    public function deactivate(){ //overrides abstract in parent LifeCycle
+    }
+
     //include the admin page in woocommerce settings
     public function includeAdminPage($settings){
         $pluginDir = plugin_dir_path( __FILE__ );
         include('Lasercommerce_Admin.php');
         $settings[] = new Lasercommerce_Admin($this->getOptionNamePrefix());
-        //$settings[] = include($pluginDir . 'Lasercommerce_Admin.php');
         return $settings;
     }
     
-    public function deactivate(){ //overrides abstract in parent LifeCycle
-        //TODO:
+    
+    //tester:
+    // public function logProductDataTabs($tabs){ //delete
+        // If(WP_DEBUG) error_log('entering LogProductTabs');
+        // foreach( $tabs as $k => $v ){
+            // If(WP_DEBUG) error_log('key: $k, value: '.serialize($v));
+        // }
+        // return $tabs;
+    // }
+    
+    // public function woocommerce_general_product_data_test_custom_field(){
+        // global $woocommerce, $post;
+        // echo '<div class="options_group pricing show_if_simple show_if_external">';
+        // woocommerce_wp_checkbox(
+            // array(
+                // 'id' => 'product_checkbox',
+                // 'wrapper_class' => 'checkbox_class',
+                // 'label' => __('Checkbox for product', 'woocommerce'),
+                // 'description' => __('Description about checkbox', 'woocommerce' )
+            // )
+        // );
+        // echo '</div>';
+    // }
+    
+    // public function maybeAddPriceFields($product_data_tabs, $tiers) {
+        // if(!empty($tiers) && !empty($product_data_tabs)){
+            
+            
+            // if(is_array($tiers)){
+                // foreach( $tiers as $tier ){
+                    
+                
+    // }
+    
+    public function addPriceField($tier){
+        //TODO: $tier error checking
+        add_action( 
+            'woocommerce_product_options_pricing',  
+            function() use ($tier){
+                woocommerce_wp_text_input( 
+                    array( 
+                        'id' => "laserphile_$tier"."_price", 
+                        'label' => __( "$tier Price", 'lasercommerce' ) . ' (' . get_woocommerce_currency_symbol() . ')', 'data_type' => 'price' 
+                    ) 
+                );    
+            }
+        );
     }
-
+    
+    public function maybeAddPriceFields($tiers){
+        if(empty($tiers)) return;
+        
+        if(is_array($tiers)){
+            foreach($tiers as $tier){
+                $this->addPriceField($tier);
+            }
+        } else {
+            $this->addPriceField($tier);
+        }
+    }
+    
     public function addActionsAndFilters() {
 
         // Add options administration page
         // http://plugin.michael-simpson.com/?page_id=47
         If(WP_DEBUG) error_log("called addActionsAndFilters\n");
         add_filter( 'woocommerce_get_settings_pages', array(&$this, 'includeAdminPage') );        
+        
+        //TODO: Make modifications to product admin
+        $this->maybeAddPriceFields( array(  "wholesale_buyer", "distributor", "expo customer" ) );
+        //$this->maybeAddPriceFields( explode(', ', $this->getOption('price_tiers')) );
+        
+        
+        //add_filter( 'woocommerce_product_data_tabs', 
+        // add_filter( 'woocommerce_product_data_tabs', array(&$this, 'logProductDataTabs'), 999, 1 );
+        // add_action( 'woocommerce_product_options_general_product_data', array(&$this, 'woocommerce_general_product_data_test_custom_field' ) );
+        
+        
+        //$product_admin_page = ?;
+        //$product_admin_price_section = ?;
+        
+        // for( explode(', ', $this->getOption('price_tiers')) as $tier){
+            
+            // //add_settings_field(???);
+        // }
+        
+        //add_filter('product visibility');
+        
+        //TODO: register product price hooks
+        //TODO: register cart hooks
+        
+        
         
         // add_action('admin_menu', array(&$this, 'addSettingsSubMenuPage'));
 
