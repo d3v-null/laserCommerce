24ab7c58de454beb19f51138309a0259bd9fe395
derwentx <derwentx@gmail.com>
2015-02-06 14:51:18 +0800

    Copyright

--- lasercommerce/lasercommerce.php
+++ lasercommerce/lasercommerce.php
@@ -44,6 +44,7 @@ All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
+    * Permission is granted to do so by the copyright holder, Laserphile
     * Redistributions of source code must retain the above copyright
       notice, this list of conditions and the following disclaimer.
     * Redistributions in binary form must reproduce the above copyright
